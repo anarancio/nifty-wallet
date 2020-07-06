658856d6b10ef138eb5d3f3cd47389994b2678fd
Rodrigo Lopetegui <rorolopetegui@gmail.com>
2020-07-06 15:28:49 -0300

    Redundant true

--- ui/app/rif/components/subDomains.js
+++ ui/app/rif/components/subDomains.js
@@ -69,7 +69,7 @@ class Subdomains extends Component {
             contentClasses={classes.content}
             actionClasses={classes.contentActions}
             text={subdomain.name}
-            enableRightChevron={true && !subdomain.action}
+            enableRightChevron={!subdomain.action}
             onRightChevronClick={() => showSubdomainDetails({
               domainName: domainInfo.domainName,
               selectedResolverAddress: domainInfo.selectedResolverAddress,
