d52e5f0e126ee93b9f581bb859f45ff7e0f29ee2
Jonathan <jonaf2103@gmail.com>
2020-07-23 14:04:31 -0300

    RIFEX-267 Fixing registration bug with no rif case
    * Using the proper loader
    * Adding future support for multi language
    
    This affects the ui and the service layers

--- ui/app/rif/pages/abstract-page.js
+++ ui/app/rif/pages/abstract-page.js
@@ -1,11 +1,18 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
+import PropTypes from 'prop-types';
+import {getLoader} from '../utils/components';
 
 export class AbstractPage extends Component {
+
+  static propTypes = {
+    t: PropTypes.func,
+  }
 
   constructor (props) {
     super(props);
     this.state = {
       loading: false,
+      loadingMessage: props.t ? props.t('Wait please...') : 'Wait please...',
     };
   }
 
@@ -18,7 +25,7 @@ export class AbstractPage extends Component {
   render () {
     const loading = this.state.loading;
     if (loading) {
-      return (<div className="app-loader"/>)
+      return getLoader(this.state.loadingMessage);
     } else {
       return this.renderPage();
     }
